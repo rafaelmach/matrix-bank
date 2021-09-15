099f43c6dea6ebb17e3c411c1a7cbea89d9fba51
Rafael Machado <85365177+rafaelmach@users.noreply.github.com>
2021-09-15 17:09:42 -0300

    Hamburger menu criado + Hook useState para efeito

--- portfolio-website/src/App.jsx
+++ portfolio-website/src/App.jsx
@@ -5,12 +5,15 @@ import Portfolio from "./components/portfolio/Portfolio"
 import Testimonials from "./components/testimonials/Testimonials"
 import Topbar from "./components/topbar/Topbar"
 import Works from "./components/works/Works"
+import { useState } from "react"
 
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false)
+
   return (
     <div className="app">
-      <Topbar/>
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       
       <div className="sections">
         
