1dcb2e5562dc054115cb319153e8f4e475da6953
Rafael Machado <85365177+rafaelmach@users.noreply.github.com>
2021-09-17 13:06:49 -0300

    Primeira Parte - Implementação do Slider

--- portfolio-website/src/components/works/Works.jsx
+++ portfolio-website/src/components/works/Works.jsx
@@ -1,9 +1,34 @@
 import "./works.scss"
+import mobile from "../../assets/mobile.png"
+import webdev2 from "../../assets/webdev2.jpg"
+import arrow from "../../assets/arrow.png"
 
 const Works = () => {
     return (
         <div className="works" id="works">
-           WORKS 
+           <div className="slider">
+               <div className="container">
+                   <div className="item">
+                       <div className="left">
+                           <div className="leftContainer">
+                               <div className="imgContainer">
+                                   <img src={mobile} alt="" />
+                               </div>
+                               <h2>Título</h2>
+                               <p>Descrição do Projeto</p>
+                               <span>Projetos</span>
+                           </div>
+                       </div>
+                       <div className="right">
+                           <img 
+                           src={webdev2} 
+                           alt="" />
+                       </div>
+                   </div>
+               </div>
+           </div>
+           <img src={arrow} className="arrow left" alt="seta do slider" />
+           <img src={arrow} className="arrow right" alt="seta do slider" />
         </div>
     )
 }
