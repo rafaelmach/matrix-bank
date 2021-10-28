e2869751fefbdfc7a8a94e50a67395a4db76500b
Rafael Machado <85365177+rafaelmach@users.noreply.github.com>
2021-10-28 18:03:21 -0300

    Criada Página externa de Login e Rota

--- matrix-bank/src/pages/SignInPage/index.js
+++ matrix-bank/src/pages/SignInPage/index.js
@@ -1,10 +1,30 @@
-
+import ScrollToTop from "../../constants/scrollToTopSignIn"
+import {
+    Container, FormWrap, Icon, FormContent,
+    Form, FormH1, FormLabel, FormInput, FormButton, Text
+} from "./SigninElements"
 
 const SignInPage = () => {
     return (
-        <div>
-           <h1> Sign in Page </h1> 
-        </div>
+        <>
+        <ScrollToTop />
+        <Container>
+            <FormWrap>
+                <Icon to="/"> Matrix Bank </Icon>
+                <FormContent>
+                    <Form action="#">
+                        <FormH1> Faça login na sua conta </FormH1>
+                        <FormLabel htmlFor="for">Email</FormLabel>
+                        <FormInput type="email" required />
+                        <FormLabel htmlFor="for">Password</FormLabel>
+                        <FormInput type="password" required />
+                        <FormButton type="submit"> Entrar </FormButton>
+                        <Text> Esqueci minha senha </Text>
+                    </Form>
+                </FormContent>
+            </FormWrap>
+        </Container>
+        </>
     )
 }
 
