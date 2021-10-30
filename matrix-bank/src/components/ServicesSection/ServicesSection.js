103aeab1c7648570333fa4415762e6c70c5f6291
Rafael Machado <85365177+rafaelmach@users.noreply.github.com>
2021-10-30 16:50:24 -0300

    Ajustes - Parte 2

--- matrix-bank/src/components/ServicesSection/ServicesSection.js
+++ matrix-bank/src/components/ServicesSection/ServicesSection.js
@@ -4,16 +4,8 @@ import { dataServiceCards } from "../../assets/Database"
 import { useState } from "react"
 import Modal from "../Modal/Modal"
 
-// DESAFIO: Criar um Modal diferente para cada um dos CARDS, criar um array com as
-// informações dos Modais, e fazer um MAP no dataModal assim como fiz nos cards.
-
-
 const ServicesSection = () => {
   const [showModal, setShowModal] = useState(false)
-
-
-  // Quando for implementar o MODAL, basta deletar o bloco ELSE e colocar a chamada 
-  // da função do MODAL no lugar do console.log, dentro do IF
 
     const onClickCard = (id) => {
       if(id === "2"){
@@ -21,9 +13,6 @@ const ServicesSection = () => {
         setShowModal(!showModal)
         console.log("CLICADO !!!", showModal)
       } 
-      // else {
-      //   console.log("Cliquei em qualquer um dos outros cards")
-      // }
     }
     
     const cards = dataServiceCards.map((item) => {
